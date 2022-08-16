31505b50b700ada2d73b7aceb9d66ecf0cbcb1cb
Wensi Rong <wr2110@nyu.edu>
2022-08-16 13:04:29 -0400

    tentative change to solve health problem2

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -103,7 +103,7 @@ class TestInventoryServer(TestCase):
 
     def test_health(self):
         """It should be healthy"""
-        response = self.client.get("/api/health")
+        response = self.client.get("/health")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(data['message'], 'OK')
